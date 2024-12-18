5b419217c8506fa73ec92cb9f31c6c4078fe3f63
Muhammad Junaidi <emje662@gmail.com>
2024-12-18 14:12:46 +0700

    create interface login & registration

--- src/registration.php
+++ src/registration.php
@@ -8,7 +8,7 @@ require './base.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login</title>
+  <title>Registrasi</title>
   <link
     rel="stylesheet"
     href="<?= BASEURL ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" />
@@ -19,9 +19,15 @@ require './base.php';
 
 <body class="d-flex align-items-center justify-content-center bg-body-secondary" style="height: 100vh;">
   <div class="p-5 bg-body-tertiary shadow rounded border-top border-4 border-primary" style="width: 40%;">
-    <h3 class="text-center pb-4">Login</h3>
+    <h3 class="text-center pb-4">Registrasi</h3>
     <form>
-
+      <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" id="email" name="email" class="form-control">
+        <div class="text-danger ps-1">
+          <?= $errors[''] ?? '' ?>
+        </div>
+      </div>
       <div class="mb-3">
         <label for="username" class="form-label">Username</label>
         <input type="text" id="username" name="username" class="form-control">
@@ -36,7 +42,14 @@ require './base.php';
           <?= $errors[''] ?? '' ?>
         </div>
       </div>
-      <button type="submit" class="btn btn-primary w-100">Login</button>
+      <div class="mb-3">
+        <label for="confirm-password" class="form-label">Konfirmasi Password</label>
+        <input type="confirm-password" id="confirm-password" name="confirm-password" class="form-control">
+        <div class="text-danger ps-1">
+          <?= $errors[''] ?? '' ?>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary w-100">Daftar</button>
     </form>
   </div>
 </body>
